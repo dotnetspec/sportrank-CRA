e29b484020c90714d04ddaecc4815e9a87440d09
dotnetspec <prmockridge@gmail.com>
2019-04-21 15:50:02 +0800

    Remove old index and dapp.js files

--- src/app/js/index.js
+++
@@ -1,6 +0,0 @@
-import EmbarkJS from '../app/src/embarkArtifacts/embarkjs';
-import DSportRank from '../app/src/embarkArtifacts/contracts/DSportRank';
-
-// import your contracts
-// e.g if you have a contract named SimpleStorage:
-//import SimpleStorage from 'Embark/contracts/SimpleStorage';
